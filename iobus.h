1a0ca149305df08f21270243a775b4afd4af70c6
FurryR <awathefox@gmail.com>
2023-01-14 19:26:44 +0800

    :sparkles: Auto start / Auto exit

--- iobus.h
+++ iobus.h
@@ -6,6 +6,10 @@
  */
 #include <poll.h>
 
+#include <list>
+#include <set>
+#include <vector>
+
 #include "./awacorn/awacorn.h"
 #include "./awacorn/promise.h"
 namespace IOBus {
@@ -18,7 +22,56 @@ typedef std::pair<pollfd, std::function<void(Awacorn::EventLoop*, pollfd*)>>
  * @brief IOBus 监听队列。
  */
 typedef class IOBus {
-  std::vector<Listener> _event;
+  std::list<Listener> _event;
+  std::set<const Listener*> _off_schedule;
+  Awacorn::EventLoop* _ev;
+  const Awacorn::Interval* _intv;
+  /**
+   * @brief 启动监听循环
+   *
+   * @return Awacorn::AsyncFn<void> 异步函数。
+   */
+  void _start() {
+    _intv = _ev->create(
+        [this](Awacorn::EventLoop* ev, const Awacorn::Interval*) -> void {
+          if (!_off_schedule.empty()) {
+            for (std::set<const Listener*>::const_iterator it =
+                     _off_schedule.cbegin();
+                 it != _off_schedule.cend(); it++) {
+              for (std::list<Listener>::const_iterator it2 = _event.cbegin();
+                   it2 != _event.cend(); it2++) {
+                if (&(*it2) == *it) {
+                  _event.erase(it2);
+                  if (_event.empty()) {
+                    _ev->clear(_intv);
+                    _intv = nullptr;
+                    break;
+                  }
+                }
+              }
+            }
+            _off_schedule.clear();
+          }
+          std::vector<pollfd> fd(_event.size());
+          size_t i = 0;
+          for (std::list<Listener>::const_iterator it = _event.cbegin();
+               it != _event.cend(); it++, i++) {
+            fd[i] = it->first;
+            fd[i].revents = 0;
+          }
+          i = 0;
+          if (poll(&fd[0], fd.size(), 1) != 0) {
+            for (std::list<Listener>::const_iterator it = _event.cbegin();
+                 it != _event.cend(); it++, i++) {
+              if (fd[i].revents != 0) {
+                it->second(ev, &fd[i]);
+              }
+            }
+          }
+        },
+        std::chrono::milliseconds(1));  // 是否合理
+  }
+  IOBus(Awacorn::EventLoop* _ev) : _ev(_ev), _intv(nullptr) {}
 
  public:
   /**
@@ -26,51 +79,27 @@ typedef class IOBus {
    *
    * @param fd 对应的 pollfd。
    * @param fn 触发时监听的函数。
-   * @return Listener* 监听 id
+   * @return const Listener* 监听 id
    */
-  Listener* wait(
+  const Listener* on(
       const pollfd& fd,
       const std::function<void(Awacorn::EventLoop*, pollfd*)>& fn) noexcept {
-    return _event.push_back(std::make_pair(fd, fn)), (Listener*)&_event.back();
+    _event.push_back(std::make_pair(fd, fn));
+    if (!_intv) {
+      _start();
+    }
+    return (const Listener*)&_event.back();
   }
   /**
    * @brief 删除监听器。
    *
    * @param id 监听 id
    */
-  void remove(const Listener* id) noexcept {
-    for (size_t i = 0; i < _event.size(); i++) {
-      if (&_event[i] == id) {
-        _event.erase(_event.cbegin() + i);
-        return;
-      }
-    }
-  }
-  /**
-   * @brief 启动监听循环
-   *
-   * @return Awacorn::AsyncFn<void> 异步函数。
-   */
-  Awacorn::AsyncFn<void> start() {
-    return [this](Awacorn::EventLoop* ev) -> void {
-      ev->create(
-          [this](Awacorn::EventLoop* ev, const Awacorn::Interval*) -> void {
-            std::vector<pollfd> fd(_event.size());
-            for (size_t i = 0; i < _event.size(); i++) {
-              fd[i] = _event[i].first;
-              fd[i].revents = 0;
-            }
-            poll(&fd[0], fd.size(), 1);
-            for (size_t i = 0; i < fd.size(); i++) {
-              if (fd[i].revents != 0) {
-                _event[i].second(ev, &fd[i]);
-              }
-            }
-          },
-          std::chrono::milliseconds(1));  // 是否合理
-    };
-  }
+  void off(const Listener* id) noexcept { _off_schedule.insert(id); }
+  friend Awacorn::AsyncFn<IOBus> create();
 } IOBus;
-IOBus global;
+Awacorn::AsyncFn<IOBus> create() {
+  return [](Awacorn::EventLoop* ev) -> IOBus { return IOBus(ev); };
+}
 }  // namespace IOBus
 #endif
